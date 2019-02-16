4544d682198d1ff8ec9b2d872ecfef9df8d5f920
Vandana Pandey <46938008+bhumijaapandey248@users.noreply.github.com>
2019-02-16 12:43:46 +0530

    Add files via upload

---
+++ assignment13.2.py
@@ -0,0 +1,12 @@
+n=int(input("enter the no. "))
+if(n%2!=0):
+    print("Weird")
+else:    
+    if(n>=2 and n<=5):
+        print("Not weird")
+    elif(n>=6 and n<=20):
+        print("Weird")
+    else:
+        print("Not Weird")
+##else:
+##    print("a")
