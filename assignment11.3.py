92eeeee151f0969dc6aafa8b023bdfa43e8dbcbb
Vandana Pandey <46938008+bhumijaapandey248@users.noreply.github.com>
2019-02-21 17:30:12 +0530

    Update assignment11.3.py

--- assignment11.3.py
+++ assignment11.3.py
@@ -1,3 +1,4 @@
+#3. Write a program in python that takes two float values from user and print the area of rectangle type casted to int.
 #type cast float into integer
 
 l=float(input("Enter the length of rectangle :"))
