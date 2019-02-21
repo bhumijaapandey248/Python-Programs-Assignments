7b27575c6961d7ce288a4c6560791590ae65154d
Vandana Pandey <46938008+bhumijaapandey248@users.noreply.github.com>
2019-02-21 17:31:18 +0530

    Update assignment11.4.py

--- assignment11.4.py
+++ assignment11.4.py
@@ -1,3 +1,5 @@
+#4. Write a program in python that takes two different string input and print them in the same line.
+
 first_str=input("Enter ur first string :")
 last_str=input("Enter ur Second string :")
 print(f"Ur first string is {first_str} and second string is {last_str}")
