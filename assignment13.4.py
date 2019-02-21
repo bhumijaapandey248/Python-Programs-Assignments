3af5ffd5e504b9b44d3892d12a504e65fa118400
Vandana Pandey <46938008+bhumijaapandey248@users.noreply.github.com>
2019-02-21 17:54:54 +0530

    Update assignment13.4.py

--- assignment13.4.py
+++ assignment13.4.py
@@ -1,10 +1,12 @@
-##Input :4 3
-##Output:1
-##	 1.3333333333333333
+#4. Read two integers and print two lines. The first line should contain integer division a//b. The second line should contain the float division a/b.
+#You don’t need to perform any rounding off or formatting operation.
+#Input: 4 3
+#Output: 1
+#1.33333333333
 
-a,b=(input("Input :")).split()
+a,b=(input()).split()
 a=int(a)
 b=int(b)
 second=a/b
 first=a//b
-print(f"Output :{first}\n\t{second}")
+print(f"{first}\n{second}")
