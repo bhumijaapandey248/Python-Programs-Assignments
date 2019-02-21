473dba8b55692039d93efd65b4d24bb59ae2ffd4
Vandana Pandey <46938008+bhumijaapandey248@users.noreply.github.com>
2019-02-21 17:29:06 +0530

    Update assignment11.2.py

--- assignment11.2.py
+++ assignment11.2.py
@@ -1,3 +1,5 @@
+#2. Write a program in python that takes two integers x, y and swap them without using third variable.
+
 x=int(input("Enter the value of x :"))
 y=int(input("Enter the value of y :"))
 x=x+y
