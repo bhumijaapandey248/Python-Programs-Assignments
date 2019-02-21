116c87ce77c7dec528f78daa2de78240201d2778
Vandana Pandey <46938008+bhumijaapandey248@users.noreply.github.com>
2019-02-21 17:39:36 +0530

    Update assignment13.1.py

--- assignment13.1.py
+++ assignment13.1.py
@@ -1,7 +1,7 @@
-#4. Write a program in python to take a string str and number num from the user and print the same string multiple times(num) with a space between each string.
-#Input: str: Ram
-#Num: 5
-#Output: Ram Ram Ram Ram Ram
+#1. There are times when all the inputs are provided in a single line and are separated by spaces. In such cases, the input() function takes the whole line as input and treats the line as a single input. We can use input().split() functionality to split the string into multiple words.
+#In this question, you will take input a single line string comprised of string, int, and float. You'll split the string and assign string to s, int to i, and float to f. Print a final string comprised of s and (i+f)
+#Input: Ram 2 2.3445
+#Output: Ram 4.3445
 
 s,i,f=input("Input :").split()
 i=int(i)
