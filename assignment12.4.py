9ac82f4763d3be95262f14db3b955ce1c458b97b
Vandana Pandey <46938008+bhumijaapandey248@users.noreply.github.com>
2019-02-21 17:37:03 +0530

    Update assignment12.4.py

--- assignment12.4.py
+++ assignment12.4.py
@@ -1,6 +1,7 @@
-##str :ram
-##n :5
-##ram ram ram ram ram 
+#4. Write a program in python to take a string str and number num from the user and print the same string multiple times(num) with a space between each string.
+#Input: str: Ram
+#Num: 5
+#Output: Ram Ram Ram Ram Ram
 
 s=(input("str :"))
 n=int(input("n :"))
