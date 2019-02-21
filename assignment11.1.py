ca2906e9dcc778d4109e704da31796ebbf9997e3
Vandana Pandey <46938008+bhumijaapandey248@users.noreply.github.com>
2019-02-21 17:27:56 +0530

    Update assignment11.1.py

--- assignment11.1.py
+++ assignment11.1.py
@@ -1,3 +1,6 @@
+#1. Write a program in python that takes three integers x, y and z as input and print their sum, product.
+
+
 a=int(input("Enter the value of a :"))
 b=int(input("Enter the value of b :"))
 c=int(input("Enter the value of c :"))
