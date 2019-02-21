04102f76aaf2b57ede6c8ab06a9463aa029459a2
Vandana Pandey <46938008+bhumijaapandey248@users.noreply.github.com>
2019-02-21 17:50:02 +0530

    Update assignment13.3.py

--- assignment13.3.py
+++ assignment13.3.py
@@ -1,13 +1,18 @@
-##Input :3 2
-##Output:5
-##	 1
-##	 6
+#3. Read two input from STDIN (Standard Input) and print three lines where:
+#a. The first line contains the sum of two numbers
+#b. The second line contains the difference of two numbers (first - second)
+#c. The third line contains the product of two number
+#Input: 3 2
+#Output: 5
+#1
+#6
+#Note: The inputs must be taken as specified i.e. the first number and second number in input must be in a single line separated by a single space.
 
-a,b=(input("Input :")).split(" ")
+a,b=(input()).split(" ")
 a=int(a)
 b=int(b)
 first=a+b
 second=a-b
 thrid=a*b
-print(f"Output :{first}\n\t{second}\n\t{thrid}")
+print(f"{first}\n{second}\n{thrid}")
 
