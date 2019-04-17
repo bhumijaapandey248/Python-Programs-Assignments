2151b3d141a3d2a8a41d098cc529f3fa652b0537
Vandana Pandey <46938008+bhumijaapandey248@users.noreply.github.com>
2019-04-17 20:58:24 +0530

    Update assignment25.2.py

--- assignment25.2.py
+++ assignment25.2.py
@@ -1,28 +1,32 @@
 #https://practice.geeksforgeeks.org/problems/pairs-with-positive-negative-values/0
 
-n = int(input())
-s = input()
-lst = s.split()
-a = list(map(int,lst))
-a1 = []
-a2 = []
-for i in a:
-    if (i > 0):
-        a1.append(i)
-        a1.sort()
-
-print(a1)
-for j in a:
-    if (j < 0):
-        a2.append(j)
-        a2.sort(reverse=True)
-print(a2)
-
-for i in range(0,n):
-    for j in range(1,n):
-        if(a1[i]==a2[j]):
-            print(a1[i],a2[j])
-        
-##            print(a[i],a[j])
-####            print(a[j])
-##print(0)
+def positive_negative(n, a):
+    a1 = []
+    a2 = []
+    for ele in a:
+        if (ele > 0):
+            a1.append(ele)
+            a1.sort()
+    l1 = len(a1)
+    for ele in a:
+        if (ele < 0):
+            a2.append(ele)
+            a2.sort(reverse=True)
+    l2 = len(a2)
+    flag = 0 # answer not found
+    for i in range(0,l1):
+        for j in range(0,l2):
+            if(a1[i]==(-1*a2[j])):
+                print(a2[j],a1[i],end=" ")
+                flag = 1 ##answer found
+    if(flag==0):
+        print(0)
+    print()
+t = int(input())
+while(t>0):
+    n = int(input())
+    s = input()
+    lst = s.split()
+    a = list(map(int,lst))
+    positive_negative(n, a)
+    t = t-1
